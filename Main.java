82c02ca08419e81f91cb2beee99ee904283e7810
LukeHartman15 <152317596+LukeHartman15@users.noreply.github.com>
2023-11-29 17:26:34 +0000

    This is from replit

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Seaver is cool");
         System.out.println("I love money mitch");
+      System.out.println("This is from replit");
     }
 }
